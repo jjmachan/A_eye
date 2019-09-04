18cc66a5dea2d489c9384ea51651f5eba768f37e
jjmachan <jamesjithin97@gmail.com>
2019-09-04 16:49:42 +0530

    created a file for accessing the model

---
+++ enet/run.py
@@ -0,0 +1,22 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+import transforms as ext_transforms
+from models.enet import ENet
+from train import Train
+from test import Test
+from metric.iou import IoU
+from args import get_arguments
+from data.utils import enet_weighing, median_freq_balancing
+import utils
+
+from main import test, train, load_dataset, predict
+
